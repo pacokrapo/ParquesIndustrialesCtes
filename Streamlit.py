990876c171d2c4a924b4a7e3f2ccd4d5fdbc3af8
unknown <fkrapo@gmail.com>
2023-08-18 12:28:32 -0300

    actualizacion streamlit

---
+++ Streamlit.py
@@ -0,0 +1,118 @@
+import folium
+import geopandas as gpd
+import json
+import streamlit as st
+from streamlit_folium import folium_static
+
+Perimetro = gpd.read_file("./ITUZAINGO/GeoJSON/Perimetro.geojson")
+AreasDelParque = gpd.read_file("./ITUZAINGO/GeoJSON/AreasDelParque.geojson")
+Arroyo = gpd.read_file("./ITUZAINGO/GeoJSON/Arroyo.geojson")
+CallePrincipal = gpd.read_file("./ITUZAINGO/GeoJSON/CallePrincipal.geojson")
+CallesSecundarias = gpd.read_file("./ITUZAINGO/GeoJSON/CallesSecundarias.geojson")
+Empresas = gpd.read_file("./ITUZAINGO/GeoJSON/Empresas.geojson")
+Parcelas = gpd.read_file("./ITUZAINGO/GeoJSON/Parcelas.geojson")
+SuperficiesCubiertas = gpd.read_file("./ITUZAINGO/GeoJSON/SuperficiesCubiertas.geojson")
+Vegetacion = gpd.read_file("./ITUZAINGO/GeoJSON/Vegetacion.geojson")
+
+
+
+# Agregar control de capas al mapa
+
+
+
+def main():
+    st.title("Mapa Parque Industrial de Ituzaingo")
+
+    mapa = folium.Map(location=[-27.619921, -56.843410], zoom_start=14)
+
+    capas = [
+        ("Perimetro", Perimetro, {'color': 'red', 'fillColor': 'transparent', 'weight': 2}),
+        ("AreasDelParque", AreasDelParque, {'color': 'red', 'fillColor': 'transparent', 'weight': 2}),
+        ("Arroyo", Arroyo, {'color': 'blue', 'fillColor': 'blue', 'weight': 0.5}),
+        ("Parcelas", Parcelas, {'color': 'orange', 'fillColor': 'yellow', 'weight': 2}),
+        ("CallePrincipal", CallePrincipal, {'color': 'red', 'fillColor': 'red', 'weight': 2}),
+        ("CallesSecundarias", CallesSecundarias, {'color': 'red', 'fillColor': 'red', 'weight': 2}),
+        ("SuperficiesCubiertas", SuperficiesCubiertas, {'color': 'blue', 'fillColor': 'blue', 'weight': 2}),
+        ("Vegetacion", Vegetacion, {'color': 'green', 'fillColor': 'green', 'weight': 0.5})
+    ]
+
+    with open('./ITUZAINGO/GeoJSON/Empresas.geojson', 'r') as geojson_file:
+        data = json.load(geojson_file)
+        empresas_features = data['features']
+
+
+    def popup_content(properties):
+        popup_html = "<ul>"
+        for key, value in properties.items():
+            if value is not None and key != 'id':
+                popup_html += f"<li><b>{key}:</b> {value}</li>"
+        popup_html += "</ul>"
+        return popup_html
+
+
+
+
+
+    # Crear una lista de elementos para el checklist
+    opciones = ["Perimetro", "Arroyo", "Parcelas", "CallePrincipal", "CallesSecundarias", "SuperficiesCubiertas", "Vegetacion", "Empresas"]
+    elementos_seleccionados = []
+
+    # Mostrar casillas de selección para cada elemento
+    for elemento in opciones:
+        seleccionado = st.sidebar.checkbox(elemento, value=True)
+        if seleccionado:
+            elementos_seleccionados.append(elemento)
+        elif elemento in elementos_seleccionados:
+            elementos_seleccionados.remove(elemento)
+    
+    folium_layers = {}
+    for nombre, capa, estilo in capas:
+        if nombre in elementos_seleccionados:
+            folium_layer = folium.GeoJson(capa, name=nombre, style_function=lambda feature, style=estilo: style)
+            folium_layer.add_to(mapa)
+            folium_layers[nombre] = folium_layer
+        else:
+            continue
+
+    if "Empresas" in elementos_seleccionados:
+        for feature in empresas_features:
+            nombre = feature['properties']['EMPRESA']
+            estilo = {'color': 'red', 'fillColor': 'transparent', 'weight': 2}
+            geojson_layer = folium.GeoJson(
+                feature,
+                name=nombre,
+                style_function=lambda x: estilo,
+                tooltip=folium.GeoJsonTooltip(fields=['EMPRESA'], labels=True),
+                popup=folium.Popup(parse_html=True, max_width=300).add_child(folium.Html(popup_content(feature['properties']), script=True))
+            )
+            geojson_layer.add_to(mapa)
+
+
+
+
+
+    selected_marker = st.selectbox("Selecciona una empresa:", [feature['properties']['EMPRESA'] for feature in empresas_features])
+
+    selected_feature = next(feature for feature in empresas_features if feature['properties']['EMPRESA'] == selected_marker)
+
+    # Obtener la ubicación de la empresa seleccionada
+    latitude = selected_feature['geometry']['coordinates'][1]
+    longitude = selected_feature['geometry']['coordinates'][0]
+
+    # Cambiar el color del pin según la empresa seleccionada
+    icon_color = "red" if selected_marker == "Empresa1" else "blue" if selected_marker == "Empresa2" else "green"
+    icon = folium.Icon(color=icon_color)
+
+    # Agregar el marcador al mapa con el ícono personalizado
+    marker = folium.Marker(location=[latitude, longitude], popup="Información de la empresa: " + selected_marker, icon=icon)
+    marker.add_to(mapa)
+
+    folium_static(mapa)
+
+    st.header("Información de la Empresa")
+
+    st.table(selected_feature['properties'])
+
+
+if __name__ == "__main__":
+    main()
